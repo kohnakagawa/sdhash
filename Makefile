05f86330ba7b5f76c09787b96fbdfcf34277c11f
Koh Nakagawa <tsunekou1019@gmail.com>
2019-03-30 09:33:08 +0900

    add python3 support

--- Makefile
+++ Makefile
@@ -71,10 +71,10 @@ swig-win-py: boost swig/python/sdbf_wrap.o swig/python/_sdbf_class.dll
 
 swig/python/sdbf_wrap.o: sdbf.i $(LIBSDBF)
 	swig -c++ -python swig/python/sdbf.i
-	g++ -std=c++0x -fPIC -c swig/python/sdbf_wrap.cxx -o swig/python/sdbf_wrap.o -I/usr/include/python2.7
+	g++ -std=c++0x -fPIC -c swig/python/sdbf_wrap.cxx -o swig/python/sdbf_wrap.o -I/usr/include/python3.5
 
 swig/python/_sdbf_class.so: swig/python/sdbf_wrap.o $(LIBSDBF)
-	g++ -shared swig/python/sdbf_wrap.o -fopenmp -L./external/stage/lib -Wl,--whole-archive -lboost_system -lboost_filesystem -lboost_thread -Wl,--no-whole-archive -lpython2.7 libsdbf.a -o swig/python/_sdbf_class.so -lcrypto -lpthread 
+	g++ -shared swig/python/sdbf_wrap.o -fopenmp -L./external/stage/lib -Wl,--whole-archive -lboost_system -lboost_filesystem -lboost_thread -Wl,--no-whole-archive -lpython3.5m libsdbf.a -o swig/python/_sdbf_class.so -lcrypto -lpthread 
 
 sdbf.i:
 
--- swig/python/test.py
+++ swig/python/test.py
@@ -1,51 +1,49 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Import our module, living in the same directory as _sdbf_class.so
 import sdbf_class
 
 # Name a few standalone objects to hash
-name = "sdbf_class.py"
-name2 = "sdbf_class.pyc"
+name1 = "test1.txt"
+name2 = "test2.txt"
 
 # Create new objects from these names, in "regular" non-block mode.
-test1 = sdbf_class.sdbf(name,0)
-test2 = sdbf_class.sdbf(name2,0)
+test1 = sdbf_class.sdbf(name1, 0)
+test2 = sdbf_class.sdbf(name2, 0)
 
 # print out some vital statistics and the hash itself
-print "test 1"
-print test1.name()
-print test1.size()
-print test1.input_size()
-print test1.to_string()
+print("test1")
+print(test1.name())
+print(test1.size())
+print(test1.input_size())
+print(test1.to_string())
 
-print "test 2"
-print test2.name()
-print test2.size()
-print test2.input_size()
-bar=test2.to_string()
+print("test2")
+print(test2.name())
+print(test2.size())
+print(test2.input_size())
+print(test1.to_string())
 
-print bar
-
-#test2.print_sdbf(test2)
+# test2.print_sdbf(test2)
 
 # Compare the two hashes and get back a score
-score = test1.compare(test2,0)
+score = test2.compare(test1,0)
 
 # display our score
-print test1.name(), " vs ", test2.name(),": ",score
+print(name1, " vs ", name2, ": ", score)
 
 # Block mode test:
-name3 = "sdbf_class.py"
+name3 = "test3.txt"
 # Note that creating this object you must give the "real size" of the block
 test3 = sdbf_class.sdbf(name3,16*1024)
 
 # Shows name
-print test3.name()
+print(test3.name())
 
 # Displays block-mode sdbf
 #print test3.tostring(); // still working on these two
 #print test3;
-print test3.to_string()
+print(test3.to_string())
 
 # This needs to be filled with an existing test file... 
 #myfile = sdbf_class.fopen("/home/candice/serverhome/newKitty.sdbf","r")
